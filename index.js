f3c6870ce12c0d4dd988a69245a91a081dcba934
khalidabdikarim99 <khalidabdikarim9@gmail.com>
2025-03-16 12:56:47 -0700

    done

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const button = document.getElementById("button");
+    
+    if (button) {
+        button.addEventListener("click", function () {
+            alert("Button was clicked!");
+        });
+    }
 }
